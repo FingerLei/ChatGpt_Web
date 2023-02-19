968ecd84243410fa6b495721c2676e35651e5cc6
FingerLei <fingerlei@126.com>
2023-02-19 19:52:32 +0800

    优化自定义

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -5,11 +5,12 @@ import { GithubSite } from '@/components/custom'
 <template>
   <div class="flex items-center">
     <div class="w-10 h-10 overflow-hidden rounded-full">
-      <img class="object-cover" src="@/assets/avatar.jpg" alt="avatar">
+      <img class="object-cover" src="@/assets/acg.jpg" alt="avatar">
+      <!--avatar.jpg-->
     </div>
     <div class="ml-2">
       <h2 class="font-bold text-md">
-        ChenZhaoYu
+        FingerLei
       </h2>
       <p class="text-xs text-gray-500">
         <GithubSite />
--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="text-center text-neutral-500">
     <span>❤️ Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <a href="https://github.com/FingerLei/ChatGpt_Web.git" target="_blank" class="text-blue-500">
+        <!--https://github.com/FingerLei/ChatGpt_Web.git https://github.com/Chanzhaoyu/chatgpt-bot-->
       GitHub
     </a>
   </div>
